6da0e8f45432476efdc9398d9470c19a7b1487a5
Zadock Maloba <malobazadock@gmail.com>
2024-04-30 18:17:58 +0300

    Implement extension of RPCClient

---
+++ RPCClient+Invoke.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonrpc;
+
+public partial class RPCClient
+{
+
+    public delegate void RPCSuccessCallback(RPCResponse response);
+    public delegate void RPCFailedCallback(RPCError error);
+
+    public string Invoke(RPCRequest request)
+    {
+        _ = PostRequests([request], async: true);
+        return request.Id ?? "";
+    }
+
+    public string Invoke(string method, object paramsObj, RPCRequestCallback callback)
+    {
+        RPCRequest request = new RPCRequest
+        {
+            Method = method,
+            Params = paramsObj,
+            Callback = callback
+        };
+        return Invoke(request);
+    }
+
+    public string Invoke(string method, object paramsObj, RPCSuccessCallback successCallback, RPCFailedCallback failedCallback)
+    {
+        return Invoke(method, paramsObj, (response) =>
+        {
+            if (response.Error != null)
+            {
+                failedCallback(response.Error);
+            }
+            else
+            {
+                successCallback(response);
+            }
+        });
+    }
+}
