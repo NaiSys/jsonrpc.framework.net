a209261bb94b793f44af4ccd804a53f5c4215141
Zadock Maloba <malobazadock@gmail.com>
2024-04-30 18:17:23 +0300

    Implement RPCResponse

---
+++ RPCResponse.cs
@@ -0,0 +1,57 @@
+﻿namespace jsonrpc;
+
+using System;
+
+public class RPCResponse
+{
+    public string? Version
+    {
+        get; set;
+    }
+    public string? Id
+    {
+        get; set;
+    }
+    public RPCError? Error
+    {
+        get; set;
+    }
+    public object? Result
+    {
+        get; set;
+    }
+
+    public RPCResponse()
+    {
+        Version = null;
+        Id = null;
+        Error = null;
+        Result = null;
+    }
+
+    public RPCResponse(object result, string id, string version)
+    {
+        Version = version;
+        Id = id;
+        Result = result;
+    }
+
+    public static RPCResponse ResponseWithError(RPCError error)
+    {
+        return new RPCResponse { Error = error };
+    }
+
+    public override string ToString()
+    {
+        return $"RPCResponse: Result={Result}, Id={Id}, Version={Version}, Error={Error}";
+    }
+
+    ~RPCResponse()
+    {
+        Version = null;
+        Id = null;
+        Error = null;
+        Result = null;
+    }
+}
+
